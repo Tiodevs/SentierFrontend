db8fbc8ac32589e007fc303ce8e2c57ca75e6c38
Tiodevs <Tiodevs>
2024-12-18 13:27:00 -0300

    feat(Home): Adicionar o analitytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,18 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <head>
-        
+        {/* Metadados comuns a todas as páginas */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        {/* Código do Google Analytics */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=G-QZR093WP8F`}></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-QZR093WP8F');
+    `}} />
       </head>
       <body>
         {children}
