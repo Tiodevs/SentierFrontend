17507297c382b38de61a97285f949b4d168e18b1
Tiodevs <santospefelipe@gmail.com>
2025-04-03 22:37:45 -0300

    feat(form):envio do form para o BD

--- src/app/components/form/index.tsx
+++ src/app/components/form/index.tsx
@@ -1,10 +1,46 @@
 "use client"
 
+import { useState } from 'react';
 import styles from './styles.module.scss'
 import { InstagramIcon, Linkedin } from "lucide-react";
+import axios from 'axios';
+import { h1 } from 'framer-motion/client';
 
 
 export function Form() {
+
+  const [nome, setNome] = useState("")
+  const [numero, setNumero] = useState("")
+  const [mensagem, setMensagem] = useState("")
+  const [returnForm, setReturnForm] = useState("")
+  const [enviado, setEnviado] = useState(false)
+
+
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    setEnviado(true)
+
+    try{
+      const response = await axios.post("http://localhost:3333/leads", {
+        nome,
+        numero,
+        mensagem
+      })
+
+      setNome('')
+      setNumero('')
+      setMensagem('')
+
+      setReturnForm("Formulario enviado com sucesso :D")
+
+    } catch (error){
+      console.error(error);
+      setReturnForm("Erro ao enviar o formulário, tente novamente...")
+    } finally{
+      setEnviado(false);
+    }
+  }
 
 
   return (
@@ -33,21 +69,25 @@ export function Form() {
 
         </div>
 
-        <form action="" className={styles.form}>
+        <form onSubmit={handleSubmit} className={styles.form}>
           <div>
             <p>Nome</p>
-            <input type="text" />
+            <input type="text" value={nome} required onChange={(e) => setNome(e.target.value)} />
           </div>
           <div>
             <p>Número</p>
-            <input type="text" />
+            <input type="text" value={numero} required onChange={(e) => setNumero(e.target.value)}/>
           </div>
           <div>
             <p>O que você precisa?</p>
-            <textarea name="" id=""></textarea>
+            <textarea name="" id="" value={mensagem} required onChange={(e) => setMensagem(e.target.value)}></textarea>
           </div>
 
-          <button type="submit">ENVIAR</button>
+          <button type="submit" disabled={enviado}>
+            {enviado ? 'Enviando...' : "Enviar"}
+          </button>
+
+          {returnForm !== "" ? <p>{returnForm}</p> : <></>}
         </form>
       </div>
 
